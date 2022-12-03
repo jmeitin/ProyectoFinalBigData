654c77f8b411041d97ab62a2ae8b629f26475a5d
Pablo Fernández <62569164+PabloFernandezAl01@users.noreply.github.com>
2022-12-03 20:30:31 +0100

    Haciendo pruebas

--- Scripts/lol.py
+++ Scripts/lol.py
@@ -1,30 +1,13 @@
 from pyspark import SparkConf, SparkContext
 from pyspark.sql.session import SparkSession
-from pyspark.sql.functions import col, from_json
-from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+from pyspark.sql.functions import from_json
 
 conf = SparkConf().setAppName('LoL')
 sc = SparkContext(conf = conf)
 spark = SparkSession(sc)
 
-df = spark.read.option("header", "true").csv("../Datasets/LOL/match.csv").drop('gameCreation', 'gameDuration', 'gameId', 'gameType', 'gameVersion', 
-                                    'mapId', 'participantIdentities', 'platformId', 'queueId', 'seasonId', 'status.message', 'status.status_code')
+df = spark.read.option("header", True).csv("../Datasets/LOL/match.csv")
 
-
-schema = StructType([
-      StructField("participantId",IntegerType(),True),
-      StructField("game",IntegerType(),True),
-  ])
-
-
-df = df.select(col("_c0"), from_json(col("participant"), schema))
-
-#json_schema = spark.read.json(df.rdd.map(lambda row: row)).schema
-
-#df = df.withColumn("participant", Functions.from_json(Functions.col("participant"), json_schema))
-
-#df.rdd.saveAsTextFile("output")
-
-df.printSchema()
-
-df.show()
+schema = spark.read.json(df.rdd.map(lambda row: row.participants)).schema
+dfCSVJSON = df.select("id", from_json("participants", schema).alias("jsonData")).select("id","jsonData.*")
+dfCSVJSON.show()
